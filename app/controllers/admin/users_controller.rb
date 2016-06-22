4b3510881763aa3409d2e1a533d1c3fdcf20f990
navy <navyboys@gmail.com>
2016-06-21 18:20:19 -0700

    pagination added through kaminari

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < AdminsController
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user_names = ["Richard", "Natalia", "Ben", "Max", "Matt", "Eamonn", "Vanessa", "Deepak", "Alexandra", "Juliana", "Terrence"]
+email = "@example.com"
+
+user_names.each do |name|
+  User.create!(firstname: name, lastname: name, password: 'password', email: name + email)
+end
